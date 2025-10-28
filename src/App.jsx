b72b8ade722b1da1ad9b5d802065809f724ab885
Sanjay T <tnsanjayy@gmail.com>
2025-10-28 15:37:38 +0300

    initial commit to dev

--- src/App.jsx
+++ src/App.jsx
@@ -17,6 +17,7 @@ function App() {
         </a>
       </div>
       <h1>Vite + React</h1>
+      <h2>This is dev branch</h2>
       <div className="card">
         <button onClick={() => setCount((count) => count + 1)}>
           count is {count}
